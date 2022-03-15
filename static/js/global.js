8b4441490b74a832bae9be7a75f61c77c7a1bbf8
Clément David <david.clement5106@gmail.com>
2022-03-15 09:51:15 +0100

    Alertes

--- static/js/global.js
+++ static/js/global.js
@@ -26,22 +26,35 @@ function initActions() {
     if (document.getElementById('edit').innerText === 'Confirmer ?') document.getElementById('edit').innerText = 'Modifier la sélection';
 }
 
+function displayAlert(type, status){
+    const alert = document.getElementById('alert');
+    const title = status !== 'success' ? 'Succès' : 'Erreur';
+    alert.innerHTML += `<div class="alert-box ${type}"><div id="alert-close"><i class="fas fa-times"></i></div><div class="alert-title">${title}</div><div class="alert-text">${status}</div></div>`;
+}
+
 function fetchLanguages(data, addOrEdit = false, remove = false) {
-    if (addOrEdit) {
-        if (document.getElementById('selected').classList.contains('editing')) document.getElementById('selected').classList.remove('editing');
-        if (document.getElementById('selected').classList.contains('adding')) document.getElementById('selected').classList.remove('adding');
-        document.getElementById('selected').innerHTML = `<td class="index">${data.index}</td><td>${data.name}</td><td>${data.birth}</td><td>${data.descr}</td>`;
-    } else if (remove) {
-        document.getElementById('selected').remove();
-        document.querySelectorAll('.index').forEach((div, key) => {
-            div.innerText = key;
-        });
-    } else if(!addOrEdit && !remove) {
-        document.getElementById('languages').innerHTML = '';
-        data.forEach((line, key) => {
-            document.getElementById('languages').innerHTML += `<tr class="language"><td class="index">${key}</td><td>${line.name}</td><td>${line.birth}</td><td>${line.descr}</td></tr>`;
-        });
-        initSelector();
+    if (data && data.type) {
+        displayAlert(data.type, data.status);
+        if (data.type === 'success') {
+            if (addOrEdit) {
+                if (document.getElementById('selected').classList.contains('editing')) document.getElementById('selected').classList.remove('editing');
+                if (document.getElementById('selected').classList.contains('adding')) document.getElementById('selected').classList.remove('adding');
+                document.getElementById('selected').innerHTML = `<td class="index">${data.index}</td><td>${data.name}</td><td>${data.birth}</td><td>${data.descr}</td>`;
+            } else if (remove) {
+                document.getElementById('selected').remove();
+                document.querySelectorAll('.index').forEach((div, key) => {
+                    div.innerText = key;
+                });
+            } else if (!addOrEdit && !remove) {
+                document.getElementById('languages').innerHTML = '';
+                data.response.forEach((line, key) => {
+                    document.getElementById('languages').innerHTML += `<tr class="language"><td class="index">${key}</td><td>${line.name}</td><td>${line.birth}</td><td>${line.descr}</td></tr>`;
+                });
+                initSelector();
+            }
+        }
+    } else {
+        displayAlert('error', 'Il y a eu un problème avec le serveur.');
     }
     initActions();
 }
