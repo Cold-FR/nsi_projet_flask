2153bf27cbca230edbd6f823a33f545e385ccac3
Cold <51854565+Cold-FR@users.noreply.github.com>
2022-03-31 10:08:33 +0200

    Use global variable for reinitialisation

--- server.py
+++ server.py
@@ -74,22 +74,25 @@ def delete():
 
 @app.route('/actions/reset', methods=['GET'])
 def reset():
+    global languages
     languages.clear()
-    languages.append({
+    languages = [
+    {
         'name': 'Java',
         'birth': 1995,
         'descr': 'Java est un langage de programmation orienté objet. Une particularité de Java est que les logiciels écrits dans ce langage sont compilés vers une représentation binaire intermédiaire qui peut être exécutée dans une machine virtuelle Java (JVM) en faisant abstraction du système d\'exploitation.'
-    })
-    languages.append({
+    },
+    {
         'name': 'JavaScript',
         'birth': 1995,
         'descr': 'JavaScript est un langage de programmation de scripts principalement employé dans les pages web interactives et à ce titre est une partie essentielle des applications web. Avec les langages HTML et CSS, JavaScript est au cœur des langages utilisés par les développeurs web.'
-    })
-    languages.append({
+    },
+    {
         'name': 'C',
         'birth': 1972,
         'descr': 'C est un langage de programmation impératif généraliste, de bas niveau. Inventé au début des années 1970 pour réécrire Unix, C est devenu un des langages les plus utilisés, encore de nos jours.'
-    })
+    }
+    ]
     return jsonify(type = 'success', status = 'La table a bien été réinitialisé.', response = languages)
 
 app.run(host='0.0.0.0', port='5001', debug=True)
